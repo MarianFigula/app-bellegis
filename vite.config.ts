c0eae19782e5ceb73f82396c47ab75978853444e
Marian Figula <majofigulam3@gmail.com>
2026-04-30 20:32:45 +0200

    feat: removed github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/app-bellegis/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
